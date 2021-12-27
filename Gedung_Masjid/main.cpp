46a1ba8e4bd7c38369bcadd9bc2616aaed833706
Esyanugerah <esyanugrah02@gmail.com>
2021-12-27 16:38:45 +0800

    Esy Menambahkan Program Kedua

--- Gedung_Masjid/main.cpp
+++ Gedung_Masjid/main.cpp
@@ -91,6 +91,182 @@ glutSolidSphere(8, 100, 100);
 glScaled (1.5,1.5,1.5);
 glPopMatrix();
 
+//Esy Anugerah
+//PEMBENTUKAN GARIS DINDING
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15,2,15.1);
+glVertex3f(-15,1.8,15.1);              //GARIS DINDING ATAS DEPAN
+glVertex3f(15,1.8,15.1);
+glVertex3f(15,2,15.1);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15,-9.8,15.1);
+glVertex3f(-15,-10,15.1);              //GARIS DINDING BAWAH DEPAN
+glVertex3f(15,-10,15.1);
+glVertex3f(15,-9.8,15.1);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15,2,-15.1);
+glVertex3f(-15,1.8,-15.1);             //GARIS DINDING ATAS BELAKANG
+glVertex3f(15,1.8,-15.1);
+glVertex3f(15,2,-15.1);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15,-9.8,-15.1);
+glVertex3f(-15,-10,-15.1);             //GARIS DINDING BAWAH BELAKANG
+glVertex3f(15,-10,-15.1);
+glVertex3f(15,-9.8,-15.1);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15.1,2,15);
+glVertex3f(-15.1,1.8,15);              //GARIS DINDING KIRI ATAS
+glVertex3f(-15.1,1.8,-15);
+glVertex3f(-15.1,2,-15);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15.1,-9.8,15);
+glVertex3f(-15.1,-10,15);              //GARIS DINDING KIRI BAWAH
+glVertex3f(-15.1,-10,-15);
+glVertex3f(-15.1,-9.8,-15);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(15.1,2,15);
+glVertex3f(15.1,1.8,15);               //GARIS DINDING KANAN ATAS
+glVertex3f(15.1,1.8,-15);
+glVertex3f(15.1,2,-15);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(15.1,-9.8,15);
+glVertex3f(15.1,-10,15);               //GARIS DINDING KANAN BAWAH
+glVertex3f(15.1,-10,-15);
+glVertex3f(15.1,-9.8,-15);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15.1,2,15.2);
+glVertex3f(-15.1,-10,15.2);           //GARIS DINDING DEPAN KIRI
+glVertex3f(-14.9,-10,15.2);
+glVertex3f(-14.9,2,15.2);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(15.1,2,15.2);
+glVertex3f(15.1,-10,15.2);            //GARIS DINDING DEPAN KANAN
+glVertex3f(14.9,-10,15.2);
+glVertex3f(14.9,2,15.2);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(-15.1,2,-15.2);
+glVertex3f(-15.1,-10,-15.2);          //GARIS DINDING BELAKANG KIRI
+glVertex3f(-14.9,-10,-15.2);
+glVertex3f(-14.9,2,-15.2);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(0.8,0.8,0.8);
+glVertex3f(15.1,2,-15.2);
+glVertex3f(15.1,-10,-15.2);           //GARIS DINDING BELAKANG KANAN
+glVertex3f(14.9,-10,-15.2);
+glVertex3f(14.9,2,-15.2);
+glEnd();
+
+glBegin(GL_QUADS);
+glVertex3f(-15.2,2,15.1);
+glVertex3f(-15.2,-10,15.1);           //GARIS DINDING KIRI DEPAN
+glVertex3f(-15.2,-10,14.9);
+glVertex3f(-15.2,2,14.9);
+glEnd();
+
+glBegin(GL_QUADS);
+glVertex3f(-15.2,2,-15.1);
+glVertex3f(-15.2,-10,-15.1);          //GARIS DINDING KIRI BELAKANG
+glVertex3f(-15.2,-10,-14.9);
+glVertex3f(-15.2,2,-14.9);
+glEnd();
+
+glBegin(GL_QUADS);
+glVertex3f(15.2,2,15.1);
+glVertex3f(15.2,-10,15.1);           //GARIS DINDING KANAN DEPAN
+glVertex3f(15.2,-10,14.9);
+glVertex3f(15.2,2,14.9);
+glEnd();
+
+glBegin(GL_QUADS);
+glVertex3f(15.2,2,-15.1);
+glVertex3f(15.2,-10,-15.1);         //GARIS DINDING KANAN DEPAN
+glVertex3f(15.2,-10,-14.9);
+glVertex3f(15.2,2,-14.9);
+glEnd();
+
+
+//PEMBENTUKAN PINTU
+glBegin(GL_QUADS);
+glColor3f(1.0,1.0,1.0);
+glVertex3f(-11,-4,15.1);
+glVertex3f(-11,-10,15.1);              //PINTU KIRI DEPAN
+glVertex3f(-7,-10,15.1);
+glVertex3f(-7,-4,15.1);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(1.0,1.0,1.0);
+glVertex3f(11,-4,15.1);
+glVertex3f(11,-10,15.1);              //PINTU KANAN DEPAN
+glVertex3f(7,-10,15.1);
+glVertex3f(7,-4,15.1);
+glEnd();
+
+glBegin(GL_QUADS);
+glColor3f(1.0,1.0,1.0);
+glVertex3f(5,-4,15.1);
+glVertex3f(5,-10,15.1);               //PINTU TENGAH DEPAN
+glVertex3f(-5,-10,15.1);
+glVertex3f(-5,-4,15.1);
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.3,0.1,0.0);
+glVertex3f(5,-4,16.1);
+glVertex3f(5,-10,16.1);               //PINTU TIANG KANAN DEPAN 3D
+glVertex3f(4,-10,16.1);
+glVertex3f(4,-4,16.1);
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.3,0.1,0.0);
+glVertex3f(5,-4,16.1);
+glVertex3f(5,-10,16.1);
+glVertex3f(5,-10,15.1);
+glVertex3f(5,-4,15.1);
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.3,0.1,0.0);
+glVertex3f(4,-4,16.1);
+glVertex3f(4,-10,16.1);
+glVertex3f(4,-10,15.1);
+glVertex3f(4,-4,15.1);
+glEnd();
+
 
 glFlush();
 glutSwapBuffers();
