edb0862cf78b5ae089f2f6866fbcbf5be4877112
AbdullAinnur <abdullainnur@gmail.com>
2021-12-29 09:28:08 +0700

    Abdull Menambahkan Komentar

--- Gedung_Masjid/main.cpp
+++ Gedung_Masjid/main.cpp
@@ -26,7 +26,7 @@ int main (int argc, char** argv){
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.0,0.0,0.0, 1.0);
 }
-
+// Abdull Tambah Perubahan
 void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
